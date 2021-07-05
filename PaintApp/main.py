87409f2e7406cb73c29cac8047540c59fbb28475
adamhalverson1 <adamhalverson41@gmail.com>
2021-07-05 08:28:31 -0500

    Added notes to main.py
    Cleaned up some spacing. added notes and a build version.

--- PaintApp/main.py
+++ PaintApp/main.py
@@ -1,4 +1,14 @@
-from random import random
+'''
+Version 1.0.1 
+    Version date: 7/5/2021
+    Initial application build
+    Base application opens and looks as expected.
+    Orange button needs to be corrected
+    Button functionality is not currently working. 
+'''
+
+#impoting needed Kivy functions
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -6,59 +16,21 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.togglebutton import ToggleButton
 
-
 class ColorBar(StackLayout):
-
-    def Red_Button(self, touch):
+    
+    def on_touch_down(self, touch):
+        color = (1, 0, 0)
         with self.canvas:
-            Color(1, 0, 0)
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+            Color(*color, mode='hsv')
+        d = 30.
+        Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+        touch.ud['line'] = Line(points=(touch.x, touch.y))
 
-    def Orange_Button(self,touch):
-        with self.canvas:
-            Color(1, 0, 0)
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-        
-    def Yellow_Button(self):
-        Color(1, 1, 0)
-
-    def Green_Button(self):
-        Color(1, 0, 0)
-
-    def Blue_Button(self):
-        Color(1, 0, 0)
-
-    def Purple_Button(self):
-        Color(1, 0, 0)
-
-    def Black_Button(self):
-        Color(1, 0, 0)
-
-    def White_Button(self):
-        Color(1, 0, 0)
-
-
-
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 class PaintApp(App):
     pass
-'''    def build(self):
-        parent = Widget()
-        self.painter = PaintWidget()
-        clearbtn = Button(text='Clear')
-        clearbtn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
-        return parent
-    
-    def clear_canvas(self, obj):
-        self.painter.canvas.clear()
-
-'''
 
 if __name__ == '__main__':
     PaintApp().run()

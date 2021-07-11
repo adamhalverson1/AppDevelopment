7dc30420c4133a19b486cf4b677139e8a3c34ef4
adamhalverson1 <adamhalverson41@gmail.com>
2021-07-11 08:16:23 -0500

    Version 1.2.2
    Updated all color ids in paint.kv

--- PaintApp/main.py
+++ PaintApp/main.py
@@ -1,21 +1,19 @@
 '''
-Version 1.2.1
+Version 1.2.2
     Version date: 7/10/2021  
-    Reverted to code from version 1.0.1. This version had a white background and ColorBar
-               
+    Added ID for redbtn in main.py paint.kv. Started the if else statement to choose the color on 
+    button click.
     
 Known Issues: 
     Orange button needs to be corrected
-    Buttons are no longer showing on the main app.
-        Working on a solution that will allow me to use the colorbar.py file
-    
-
+    Button click does not select the color to be used on the canvas
          
 Things to Try:
-    Create a class for each color in the ColorBar. 
-    In the App class, create a variable for a StackLayout and add each button color with add_widget.
+        In the App class, create a variable for a StackLayout and add each button color with add_widget.
     Reverted to old version to test. 
         Old version had the desired background color and button bar. 
+    Trying Toggle button behavior and adding an if else statement to choose the color.
+        Each color will be assigned an ID that can be called in the if else statement. 
        
 '''
 
@@ -29,20 +27,31 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.graphics import Color, Ellipse, Line
 
-
 class ColorBar(StackLayout):
     
-    def on_touch_down(self, touch):
-        color = (1, 0, 0)
-        with self.canvas:
-            Color(*color, mode='hsv')
-        d = 30.
-        Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-        touch.ud['line'] = Line(points=(touch.x, touch.y))
+    def __init__(self, **kwargs):
+        super(ColorBar, self).__init__(**kwargs)
+        
+    def btn_id(self, *args):
+        print()     
+   
+    def on_state(self, widget, value, touch):
+        
+        if self.ids == 'redbtn' and value == 'down':
+            color = (1,0,0)
+            with self.canvas:
+                Color(*color, mode='hsv')
+            d = 10.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))            
+        else:
+            color = (1,0,0)
+            with self.canvas:
+                Color(*color, mode='hsv')
+            d = 10.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))  
 
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y] 
-    
 class PaintApp(App):
     pass
 

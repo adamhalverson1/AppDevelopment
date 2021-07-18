8acbfd2013935861d1b60de0d30e1f4bcd89c23c
adamhalverson1 <adamhalverson41@gmail.com>
2021-07-18 06:54:58 -0500

    Version 1.2.3
    Made some changes to ToggleButton functionality. Color selection still not working.

--- PaintApp/main.py
+++ PaintApp/main.py
@@ -1,19 +1,17 @@
 '''
-Version 1.2.2
-    Version date: 7/10/2021  
-    Added ID for redbtn in main.py paint.kv. Started the if else statement to choose the color on 
-    button click.
+Version 1.2.3
+    Version date: 7/17/2021  
+    Working on getting the Buttons to print the button color has been pressed. 
+    Once I can figure out that interaction I will hopefully be able to select the paint color using
+    the button.
+    
     
 Known Issues: 
     Orange button needs to be corrected
     Button click does not select the color to be used on the canvas
          
 Things to Try:
-        In the App class, create a variable for a StackLayout and add each button color with add_widget.
-    Reverted to old version to test. 
-        Old version had the desired background color and button bar. 
-    Trying Toggle button behavior and adding an if else statement to choose the color.
-        Each color will be assigned an ID that can be called in the if else statement. 
+    Trying to print some text with the button clicks. 
        
 '''
 
@@ -27,34 +25,43 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.graphics import Color, Ellipse, Line
 
-class ColorBar(StackLayout):
+#Creating the Paint Brush widget
+class PaintWidget(Widget):
+    def paint_color(self):
+    color = ToggleButton()
+        if self.value:
+            result.state = 'down'
     
-    def __init__(self, **kwargs):
-        super(ColorBar, self).__init__(**kwargs)
-        
-    def btn_id(self, *args):
-        print()     
-   
-    def on_state(self, widget, value, touch):
-        
-        if self.ids == 'redbtn' and value == 'down':
-            color = (1,0,0)
-            with self.canvas:
-                Color(*color, mode='hsv')
-            d = 10.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))            
-        else:
-            color = (1,0,0)
-            with self.canvas:
-                Color(*color, mode='hsv')
-            d = 10.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))  
+    #Changing the color to Red if the Red Button is selected.
+        self.ids.redbtn.text = 'redbtn has been pressed.'
+        self.ids.orangebtn.text = 'orangebtn has been pressed.'
+        self.ids.yellowbtn.text = 'yellowbtn has been pressed.'
+        self.ids.greenbtn.text = 'greenbtn has been pressed.'
+        self.ids.bluebtn.text = 'bluebtn has been pressed.'
+        self.ids.purplebtn.text = 'purplebtn has been pressed.'
+        self.ids.blackbtn.text = 'blackbtn has been pressed.'
+        self.ids.whitebtn.text = 'whitebtn has been pressed.'
 
+#Creating the Color Bar to select the colors and the canvas. 
+class ColorBar(StackLayout):
+
+    #Telling the app what to do when there is touch input
+    def on_touch_down(self, touch):
+        paint = PaintWidget()
+        paint.center = touch.pos
+        self.add_widget(paint)
+
+    #Telling the app what to do when the touch input moves. 
+    def on_touch_move(self, touch):
+        paint = PaintWidget()
+        paint.center = touch.pos
+        self.add_widget(paint)
+                
+#Creating the Paint App
 class PaintApp(App):
     pass
 
+#Calling the App 
 if __name__ == '__main__':
     PaintApp().run()
    

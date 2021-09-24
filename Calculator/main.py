d08ec34a1525866f8aa28df930dd5d945b836db9
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2021-09-24 15:26:28 -0500

    Version 1.0
    Added working buttons and addition functionality.

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,3 +1,13 @@
+'''
+Version: 1.0 
+Version Date: 9/24/2021
+
+Version Notes:
+    Created the calculator GUI. 
+    Made functioning numnber buttons.
+    Added addition functionality. 
+
+'''
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
@@ -12,6 +22,54 @@ Builder.load_file('calculator.kv')
 class MyLayout(Widget):
     def clear(self):
         self.ids.calc_input.text = '0'
+
+    def button_press(self, button):
+        prior = self.ids.calc_input.text
+
+        if prior == "0":
+            self.ids.calc_input.text = ''
+            self.ids.calc_input.text = f'{button}'
+        else:
+            self.ids.calc_input.text = f'{prior}{button}'
+
+    def add(self):
+        prior = self.ids.calc_input.text
+        
+        self.ids.calc_input.text = f'{prior}+'
+
+    def subtract(self):
+        prior = self.ids.calc_input.text
+        
+        self.ids.calc_input.text = f'{prior}-'
+
+    def multiply(self):
+        prior = self.ids.calc_input.text
+        
+        self.ids.calc_input.text = f'{prior}*'
+
+    def divide(self):
+        prior = self.ids.calc_input.text
+        
+        self.ids.calc_input.text = f'{prior}/'
+
+    def equals(self):
+        prior = self.ids.calc_input.text
+
+        if "+" in prior:
+            num_list = prior.split("+")
+            answer = 0
+            for number in num_list:
+                answer = answer + int(number)
+            
+            self.ids.calc_input.text = str(answer)
+
+        elif "-" in prior:
+            num_list = prior.split("-")
+            answer = 0
+            for number in num_list:
+                answer = answer - int(number)
+            
+            self.ids.calc_input.text = str(answer)
 
 class CalculatorApp(App):
     def build(self):

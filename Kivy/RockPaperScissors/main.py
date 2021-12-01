88d6a8fbf674613ad22752131615740463a69472
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2021-12-01 12:07:04 -0600

    Version  1.2
    Fully functional Rock Paper Scissors game.

--- Kivy/RockPaperScissors/main.py
+++ Kivy/RockPaperScissors/main.py
@@ -1,16 +1,25 @@
 '''
 Rock Paper Scissors 
-    Version: 1.1
-    Version Date: 11/29/2021
+    Version: 1.2
+    Version Date: 12/1/2021
 
 Version Details:
     This is the initial build of a rock paper scissors game built using python and kivy. 
     The basic template for a Kivy app has been constructed. No functionality has been added at this time.
     Added buttons to the application using the kv file. 
     Added the code for the Rock Paper Scissors game.
+    Created functions that will print rock, paper and scissors to the console when pressed.
+        I should be able to use this function to pass the selection to the user variable.
+    Created a computer variable that randomly selects rock, paper or scissors. 
+    Coded the rock paper scissors game. Results are printed to the console. 
+        This is fully functional. 
+
+        
 
 To Do
-    Add the functionality to select Rock Paper or Scissors using the buttons.
+    Write the results to the GUI
+    Keep count of the score and write that to the GUI. 
+    Make the GUI more appealing. 
 
 '''  
 
@@ -27,32 +36,31 @@ Builder.load_file('RockPaperScissors.kv')
 
 #Creating the class for Rock Paper Scissors. This is left intentionally blank at this time. 
 class RockPaperScissors (Widget):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if self.ids.btn == "rock":
-            user = 'rock'
-        elif self.ids.btn == 'paper':
-            user = 'paper'
-        elif self.ids.btn == 'scissors':
-            user = 'scissors'
 
-    #Creating the function for the Rock Paper Scissors game. 
-    def playGame():
-        #User selection is based on the selected button
-        user = '''Button selection'''
-        
-        #computer selection is a random selection. 
-        computer = computer = random.choice(['ROCK', 'PAPER', 'SCISSORS'])
+    #Function for running the rock paper scissors game using player selection by button and random computer selection.
+    def button_press (self, user_choice):
+        self.user_choice = user_choice
+        print(user_choice)
+
+        #Creating the computer selection variable, it will randomly select rock, paper or scissors. 
+        computer = random.choice(['ROCK', 'PAPER', 'SCISSORS'])
+        print(computer)
 
         #Checking to see if the user and computer have the same selection
-        if user == computer:
-            return 'It is a tie game'
-
-        if is_win(user, computer):
-            return "You Won!"
+        if user_choice == computer:
+            print ("It is a tie game")
         
-        return "You lost"
+        #Checking to see if the user is the winner 
+        elif is_win(user_choice, computer):
+            print("You Won!")
+        #Computer won message. 
+        else:
+            print("You Lost")
 
+#Function for determining the winner
 def is_win(player, opponent):
     #returns true if the player wins 
     if (player == "ROCK" and opponent == "SCISSORS") or (player == "PAPER" and opponent == "ROCK") or (player == "SCISSORS" and opponent == "PAPER"):
@@ -60,6 +68,7 @@ def is_win(player, opponent):
 
 #Creating the game app class. 
 class Game(App):
+
     def build(self):
         return RockPaperScissors()
 

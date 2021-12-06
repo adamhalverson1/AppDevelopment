9de51c913538b018cfd4da4ab0b56d26eade2fc8
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2021-12-06 15:55:51 -0600

    Version 1.5
    Added Images to the rock, paper and scissors buttons. Added Reset button. Modified fonts.

--- Kivy/RockPaperScissors/main.py
+++ Kivy/RockPaperScissors/main.py
@@ -1,28 +1,32 @@
 '''
 Rock Paper Scissors 
-    Version: 1.4
-    Version Date: 12/3/2021
+    Version: 1.5
+    Version Date: 12/6/2021
 
 Version Details:
     This is the initial build of a rock paper scissors game built using python and kivy. 
     The basic template for a Kivy app has been constructed. No functionality has been added at this time.
     Added buttons to the application using the kv file. 
     Added the code for the Rock Paper Scissors game.
-    Created functions that will print rock, paper and scissors to the console when pressed.
-        I should be able to use this function to pass the selection to the user variable.
     Created a computer variable that randomly selects rock, paper or scissors. 
-    Coded the rock paper scissors game. Results are printed to the console. 
-        This is fully functional. 
     Added a slate grey background
     Results are being writted to the GUI by updating the label text. 
     Added labels that display the computer and user choice. 
     Adjust label positions.
     Score is being kept and is displayed in the GUI
     Changed the verbiage for the won and lost messages to include the player and computer choices. 
+    Added games play count 
+    Added Rock Paper and Scissors images to the buttons
+        Removed Rock, Paper and Scissors text from the buttons
+    Added spacing in between the buttons. They no longer look like one larger button 
+    Adjusted font sizes. 
+    Added a reset button that brings all of the values (games played, player and computer scores) back to 0
 
-To Do
+To Do:
     Make the GUI more appealing. 
-    Rock Paper and Scissors icons for the buttons 
+
+Bugs:
+    
 
 '''  
 
@@ -52,10 +56,25 @@ class RockPaperScissors (Widget):
         computer = random.choice(['Rock', 'Paper', 'Scissors'])
         self.ids.cchoice.text = computer
 
+        #If the reset button is selected all values are returned to 0
+        if user_choice == "Reset":
+            self.ids.games.text = "0"
+            self.ids.player.text = "0"
+            self.ids.computer.text = "0"   
+        
         #Checking to see if the user and computer have the same selection
-        if user_choice == computer:
+        elif user_choice == computer:
             self.ids.message.text = "It's a tie game, you and the computer both picked " + user_choice
-            
+
+            #Creating a variable for games played
+            games = self.ids.games.text
+            #Converting the number of games to an integer
+            games = int(games) + 1
+            #Converting the number of games back to a string 
+            games = str(games)
+            #Updating the label with the new player score 
+            self.ids.games.text = games            
+
         
         #Checking to see if the user is the winner 
         elif is_win(user_choice, computer):
@@ -69,6 +88,15 @@ class RockPaperScissors (Widget):
             player_score = str(player_score)
             #Updating the label with the new player score 
             self.ids.player.text = player_score
+
+            #Creating a variable for games played
+            games = self.ids.games.text
+            #Converting the number of games to an integer
+            games = int(games) + 1
+            #Converting the number of games back to a string 
+            games = str(games)
+            #Updating the label with the new player score 
+            self.ids.games.text = games     
 
             
             
@@ -84,6 +112,15 @@ class RockPaperScissors (Widget):
             computer_score = str(computer_score)
             #Updating the label with the new player score 
             self.ids.computer.text = computer_score
+
+            #Creating a variable for games played
+            games = self.ids.games.text
+            #Converting the number of games to an integer
+            games = int(games) + 1
+            #Converting the number of games back to a string 
+            games = str(games)
+            #Updating the label with the new player score 
+            self.ids.games.text = games     
             
 
 #Function for determining the winner

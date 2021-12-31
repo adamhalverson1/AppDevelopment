db3b060e74b9b44aa62f344924614da5740a9c19
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2021-12-31 09:48:52 -0600

    Update main.py
    updated code for google admob

--- Kivy/RockPaperScissors/main.py
+++ Kivy/RockPaperScissors/main.py
@@ -1,7 +1,7 @@
 '''
 Rock Paper Scissors 
-    Version: 1.5
-    Version Date: 12/6/2021
+    Version: 1.6
+    Version Date: 12/12/2021
 
 Version Details:
     This is the initial build of a rock paper scissors game built using python and kivy. 
@@ -21,19 +21,32 @@ Version Details:
     Added spacing in between the buttons. They no longer look like one larger button 
     Adjusted font sizes. 
     Added a reset button that brings all of the values (games played, player and computer scores) back to 0
+    UI modifications have been made 
+        Buttons have been rasied to accomodate a banner add at the bottom of the app
+        Size has been changed from root. hieght/width to 800x500px - this will hopefully be better for mobile
+    Added code for AdMob using KivMob. 
+        Will need to package the app to do further testing.
 
 To Do:
-    Make the GUI more appealing. 
-
-Bugs:
+    Add Google AdMob support using KivMob
+        Code has been added. Tests do not show ads. Not sure if this is expected or a bug. Need to wait until a production version is ready. 
+        Buttons on the buttom of the app have been moved up to support a lower add banner placement. 
+            Placement may need to be adjusted more once ads are shown on app.
+    All app content is showing in the lower left of the mobile screen. 
+        Need to determine why and how to correct.
     
 
 '''  
 
-#Importing the needed Kivy modules.
+#Importing KivMob for Google AdMob support 
+from io import SEEK_CUR
+from kivmob import KivMob
+
+#Importing the Kivy modules for the application.
 from kivy.app import App
 from kivy.lang.builder import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 
 #importing Random module.
 import random
@@ -43,10 +56,14 @@ Builder.load_file('RockPaperScissors.kv')
 
 #Creating the class for Rock Paper Scissors. This is left intentionally blank at this time. 
 class RockPaperScissors (Widget):
+    ads = KivMob("ca-app-pub-3345304889044888~9817784347".APP)
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+        super(Widget, self).__init__(**kwargs)
+        self.ads.new_banner("ca-app-pub-3345304889044888/6748235043".BANNER,False)
+        self.ads.request_banner()
+        self.ads.show_banner()
+         
     #Function for running the rock paper scissors game using player selection by button and random computer selection.
     def button_press (self, user_choice):
         self.user_choice = user_choice
@@ -120,14 +137,15 @@ class RockPaperScissors (Widget):
             #Converting the number of games back to a string 
             games = str(games)
             #Updating the label with the new player score 
-            self.ids.games.text = games     
-            
+            self.ids.games.text = games      
 
 #Function for determining the winner
 def is_win(player, opponent):
     #returns true if the player wins 
     if (player == "Rock" and opponent == "Scissors") or (player == "Paper" and opponent == "Rock") or (player == "Scissors" and opponent == "Paper"):
         return True
+
+   
 
 #Creating the game app class. 
 class Game(App):
